7d390bf3a8e63fafd39c698a6811d944463bd2c7
John-Debon <matudbonis@gmail.com>
2018-11-11 14:58:00 -0300

    refactoring done (guia 2)

--- js/listado.js
+++ js/listado.js
@@ -18,69 +18,55 @@ Listado.prototype.calificarRestaurant = function(id, calificacion) {
 
 //Dado un id, busca el objeto del listado que tiene ese id
 Listado.prototype.buscarRestaurante = function(id) {
-    for (var i = 0; i < this.restaurantes.length; i++) {
-        if (this.restaurantes[i].id === id) {
-            return this.restaurantes[i];
-        }
+    let resultado = this.restaurantes.find((element) => element.id === id);
+    if (typeof resultado === 'object') {
+        return resultado;
+    } else {
+        return 'No se ha encontrado ningún restaurant';
     }
-    return 'No se ha encontrado ningún restaurant';
 };
 
-//Obtiene todas las ciudades de los restaurantes sin repetidos
-Listado.prototype.obtC = function() {
-    //Array donde se van a ir agregando las ciudades (van a estar repetidas)
-    var c = [];
-    //Se recorre el array de restaurantes y se va agregando al array creado, todas las ubicaciones o ciudades encontradas
-    for (var i = 0; i < this.restaurantes.length; i++) {
-        c.push(this.restaurantes[i].ubicacion);
-    }
-    //Se crea un nuevo array donde se van a agregar las ciudades pero sin repetirse
-    var c2 = c.filter(function(elem, index, self) {
+function eliminarRepeticion(array) {
+    let arrayFiltrado = array.filter(function(elem, index, self) {
         return index === self.indexOf(elem);
     });
+    return arrayFiltrado;
+}
 
-    return c2.sort();
+//Obtiene todas las ciudades de los restaurantes sin repetidos
+Listado.prototype.obtUbicacion = function() {
+    //Array donde se van a ir agregando las ciudades (van a estar repetidas)
+    var arrayUbicacion = this.restaurantes.map((element) => element.ubicacion);
+    let arrayFiltrado = eliminarRepeticion(arrayUbicacion);
+
+    return arrayFiltrado.sort();
 };
 
 //Obtiene todos los rubros de los restaurantes sin repetidos. Su funcionamiento es similar a obtC()
-Listado.prototype.obtR = function() {
-    var r = [];
-    for (var i = 0; i < this.restaurantes.length; i++) {
-        r.push(this.restaurantes[i].rubro);
-    }
+Listado.prototype.obtRubro = function() {
+    var arrayRubro = this.restaurantes.map((element) => element.rubro);
+    let arrayFiltrado = eliminarRepeticion(arrayRubro);
 
-    var r2 = r.filter(function(elem, index, self) {
-        return index === self.indexOf(elem);
-    });
-
-    return r2.sort();
+    return arrayFiltrado.sort();
 };
 
 //Obtiene todos los horarios de los restaurantes (sin repetidos). Está funcionalidad es un poco más compleja ya que un restaurante
 //tiene un array de horarios. Al buscarlos todos vamos a pasar a tener un array de arrays que luego vamos a tener que
 //convertir en uno solo
-Listado.prototype.obtH = function() {
+Listado.prototype.obtHorarios = function() {
     //En este array se van a cargar los arrays de horarios, que luego vamos convertir en un solo array
-    var arregloH = [];
-    //Recorremos el array de restaurantes y vamos agregando todos los array de horarios
-    for (var i = 0; i < this.restaurantes.length; i++) {
-        arregloH.push(this.restaurantes[i].horarios);
-    }
+    var arregloHorarios = this.restaurantes.map((element) => element.horarios);
 
     //En este arreglo vamos a poner todos los horarios, uno por uno
-    var h = [];
-    arregloH.forEach(function(a) {
+    var arrayHorariosNuevo = [];
+    arregloHorarios.forEach(function(a) {
         a.forEach(function(hor) {
-            h.push(hor);
+            arrayHorariosNuevo.push(hor);
         });
     });
+    let arrayFiltrado = eliminarRepeticion(arrayHorariosNuevo);
 
-    //En este arreglo vamos a poner todos los horarios pero sin repetidos
-    var h2 = h.filter(function(elem, index, self) {
-        return index === self.indexOf(elem);
-    });
-
-    return h2.sort();
+    return arrayFiltrado.sort();
 };
 
 //Función que recibe los filtros que llegan desde el HTML y filtra el arreglo de restaurantes.
--- js/restaurant.js
+++ js/restaurant.js
@@ -9,12 +9,8 @@ var Restaurant = function(id, nombre, rubro, ubicacion, horarios, imagen, califi
 };
 
 Restaurant.prototype.reservarHorario = function(horarioReservado) {
-    for (var i = 0; i < this.horarios.length; i++) {
-        if (this.horarios[i] === horarioReservado) {
-            this.horarios.splice(i, 1);
-            return;
-        }
-    }
+    let arrayHorariosNuevo = this.horarios.filter((element) => element !== horarioReservado);
+    this.horarios = arrayHorariosNuevo;
 };
 
 Restaurant.prototype.calificar = function(nuevaCalificacion) {
@@ -23,15 +19,22 @@ Restaurant.prototype.calificar = function(nuevaCalificacion) {
     }
 };
 
+function sumatoria(numeros) {
+    let sumatoria = 0;
+    numeros.forEach((element) => {
+        sumatoria += element;
+    });
+    return sumatoria;
+}
+
+function promedio(numeros) {
+    return sumatoria(numeros) / numeros.length;
+}
+
 Restaurant.prototype.obtenerPuntuacion = function() {
     if (this.calificaciones.length === 0) {
         return 0;
     } else {
-        var sumatoria = 0;
-        for (var i = 0; i < this.calificaciones.length; i++) {
-            sumatoria += this.calificaciones[i];
-        }
-        var promedio = sumatoria / this.calificaciones.length;
-        return Math.round(promedio * 10) / 10;
+        return Math.round(promedio(this.calificaciones));
     }
 };
