2468b0a00a7a2ffd461f47c9600bba81a3042e3d
John-Debon <matudbonis@gmail.com>
2018-11-18 20:29:23 -0300

    functions done

--- js/reservas.js
+++ js/reservas.js
@@ -12,14 +12,17 @@ Reserva.prototype.calcularPrecioBase = function calcularPrecioBaseFn() {
 Reserva.prototype.calcularPrecioFinal = function calcularPrecioFinalFn() {
     let descuentoPersona;
     let descuentoCodigo;
-
+    let adicionalHorario;
+    let adicionalFinde;
     //DESCUENTOS POR PERSONA
-    if (this.personas >= 4 || this.personas <= 6) {
+    if (this.personas >= 4 && this.personas <= 6) {
         descuentoPersona = (this.calcularPrecioBase() * 5) / 100;
-    } else if (this.personas >= 7 || this.personas <= 8) {
+    } else if (this.personas >= 7 && this.personas <= 8) {
         descuentoPersona = (this.calcularPrecioBase() * 10) / 100;
     } else if (this.personas > 8) {
         descuentoPersona = (this.calcularPrecioBase() * 15) / 100;
+    } else {
+        descuentoPersona = 0;
     }
 
     //DESCUENTOS POR CODIGO
@@ -29,9 +32,28 @@ Reserva.prototype.calcularPrecioFinal = function calcularPrecioFinalFn() {
         descuentoCodigo = (this.calcularPrecioBase() * 15) / 100;
     } else if (this.codigo === 'DES200') {
         descuentoCodigo = 200;
+    } else {
+        descuentoCodigo = 0;
     }
 
-    //TODO: DESCUENTOS ADICIONALES POR HORARIO
+    //ADICIONAL POR HORARIO
+    if (this.horario.getHours() >= 13 && this.horario.getHours() <= 14) {
+        adicionalHorario = descuentoPersona = (this.calcularPrecioBase() * 5) / 100;
+    } else {
+        adicionalHorario = 0;
+    }
 
-    return this.calcularPrecioBase() - descuentoPersona - descuentoCodigo;
+    //ADICIONAL POR FIN DE SEMANA
+    if (this.horario.getDay() >= 5 && this.horario.getDay() <= 7) {
+        adicionalFinde = (this.calcularPrecioBase() * 10) / 100;
+    } else {
+        adicionalFinde = 0;
+    }
+    return (
+        this.calcularPrecioBase() -
+        descuentoPersona -
+        descuentoCodigo +
+        adicionalHorario +
+        adicionalFinde
+    );
 };
