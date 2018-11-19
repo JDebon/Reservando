5a6652c8b5085c03575e93b9669256b5ca776c38
Debon <matudbonis@gmail.com>
2018-11-18 21:10:34 -0300

    bug fix

--- js/testResevas.js
+++ js/testResevas.js
@@ -1,9 +1,16 @@
 describe('TDD', function() {
     it('Calculo de reserva', function() {
-        var reserva1 = new Reserva(new Date(2018, 7, 24, 11, 00), 8, 350, 'DES1');
-        let precioBase = reserva1.calcularPrecioBase();
-        let precioFinal = reserva1.calcularPrecioFinal();
+        var reserva = new Reserva(new Date(2018, 7, 27, 14, 100), 2, 150, 'DES200');
+        let precioBase = reserva.calcularPrecioBase();
+        let precioFinal = reserva.calcularPrecioFinal();
+        expect(precioBase).to.be.equal(300);
+        expect(precioFinal).to.be.equal(100);
+    });
+    it('Calculo de reserva', function() {
+        var reserva = new Reserva(new Date(2018, 7, 24, 11, 00), 8, 350, 'DES1');
+        let precioBase = reserva.calcularPrecioBase();
+        let precioFinal = reserva.calcularPrecioFinal();
         expect(precioBase).to.be.equal(2800);
-        expect(precioFinal).to.be.equal(2310);
+        expect(precioFinal).to.be.equal(2450);
     });
 });
